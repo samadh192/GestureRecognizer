3c83300ed0ca868bd654b84c9934e8d22d1e0d28
samadh192 <azathabdulsamadh@gmail.com>
2023-05-02 21:47:44 -0400

    Added icon for app

--- app/src/main/java/com/example/gesturerecognizer/Activity/MainActivity.java
+++ app/src/main/java/com/example/gesturerecognizer/Activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.gesturerecognizer.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.widget.Button;
 import android.widget.TextView;
